898780b84bbe52fdbe67fa681333c94f98fc1bc9
Iamchandanmehto <138262306+Iamchandanmehto@users.noreply.github.com>
2024-03-02 11:48:19 +0000

    string number covnersion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*********************Operations*************************
+
+let value = 3
+let nagValue = -value
+
+//console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+
+
+let str1 = "hello"
+let str2 = " chandan"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
