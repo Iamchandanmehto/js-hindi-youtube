96ec75e8f91bdeb443ac543d97a250f9be95ae31
Iamchandanmehto <138262306+Iamchandanmehto@users.noreply.github.com>
2024-03-19 11:01:23 +0000

    conversion of data type

---
+++ 01_basics/07_conversionOperation.js
@@ -0,0 +1,26 @@
+ let score = "chandan"
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+ // "33" =>33
+ // "33abc" => NaN
+ // true => 1; false=> 0
+
+let isLoggedIn = "chandan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+//"chandan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
+
