ea1d0a4f1dd148bcec23a6aae9a03ac733e65731
Iamchandanmehto <138262306+Iamchandanmehto@users.noreply.github.com>
2024-03-22 11:53:40 +0000

    nums and maths.js

---
+++ 01_basics/10_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 100
+
+const balance  = new Number(100)
+//console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 198.8378
+// console.log(otherNumber.toPrecision(3));
+
+const anotherNumber = 10000000000000000
+// console.log(anotherNumber.toLocaleString(`en-IN`));
+
+
+
+
+//+++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(8, 5, 10, 2, 9));
+// console.log(Math.max(3, 5, 5, 8, 19, 0));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()*(max - min + 1)) +min) 
