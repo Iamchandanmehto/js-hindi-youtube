9213ec92ed0fe38a0a6491c2761c61dd4305f1fb
Iamchandanmehto <138262306+Iamchandanmehto@users.noreply.github.com>
2024-04-13 10:13:23 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,55 @@ if (true) {
 let a = 10
 const b = 20
 var c = 30
-console.log("INNER:", a);
+// console.log("INNER:", a);
 }
 
- console.log(a);
+//  console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one(){
+    const username = "chandan"
+
+    function two(){
+        const website = "github"
+        console.log(username);
+    }
+
+    // console.log(website);
+    
+//   two()
+    
+}
+
+// one()
+
+if (true) {
+    const username = "chandan"
+    if (username === "chandan") {
+        const website = " github"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++ interesting +++++++++++++++++
+
+console.log(addone(4))
+function addone(num){
+    return num + 1
+}
+
+
+
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
+
